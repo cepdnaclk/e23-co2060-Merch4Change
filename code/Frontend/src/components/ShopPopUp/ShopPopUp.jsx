f2691fb0caac00fa8a7a56a0ef3f169f2fd5ca43
malith-sandanayake <malithsandanayake@gmail.com>
2026-03-05 21:44:39 +0530

    Create a dev branch

---
+++ code/Frontend/src/components/ShopPopUp/ShopPopUp.jsx
@@ -0,0 +1,11 @@
+import "./ShopPopUp.css";
+
+function ShopPopUp() {
+    return (<>
+        <div className="pop-up">
+            <p>hello my product</p>
+        </div>
+    </>);
+}
+
+export default ShopPopUp;
