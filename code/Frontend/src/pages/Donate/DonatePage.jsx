58f769eb3db22e04f6e3c0c4a3abf4978022fe07
malith-sandanayake <malithsandanayake@gmail.com>
2026-09-26 10:34:04 +0530

    Fix syntax error missing closing tag in DonatePage

--- code/Frontend/src/pages/Donate/DonatePage.jsx
+++ code/Frontend/src/pages/Donate/DonatePage.jsx
@@ -527,6 +527,8 @@ export default function DonatePage() {
                     </form>
                   </div>
                 )}
+              </>
+            )}
 
           </div>
         </main>
